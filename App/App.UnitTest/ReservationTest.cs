8d3e5221f90dd297810eced417851b4944bfbb8a
Tekeshwar <tekeshpixel@gmail.com>
2018-10-11 11:02:36 +0530

    Created unit test project

---
+++ App/App.UnitTest/ReservationTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using App.App_Data;
+namespace App.UnitTest
+{
+    [TestClass]
+    public class ReservationTest
+    {
+        [TestMethod]
+        public void canBeCancelledBy_UserIsAdmin_ReturnsTrue()
+        {
+            //MathodName_Scenario_ExpectedBehavior()
+
+            //Arrange ----Initialaize object
+            Reservation reservation = new Reservation();
+
+            //Act
+            var result = reservation.CanBeCancelledBy(new User { IsAdmin = true });
+
+            //Assert
+            Assert.IsTrue(result);
+        }
+    }
+}
---
+++ App/App/App_Data/Reservation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.App_Data
+{
+   public class Reservation
+    {
+       public User MadeBy { get; set; }
+       /// <summary>
+       /// Admin and user can cancel reservatoin (ture) and other can not (false)
+       /// </summary>
+       /// <param name="user"></param>
+       /// <returns></returns>
+       public bool CanBeCancelledBy(User user)
+       {
+           return (user.IsAdmin || MadeBy == user);
+       }
+    }
+}
